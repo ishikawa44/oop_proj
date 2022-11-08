7eb98d21b98e1f835d10effc8cc95e9f52cb67d1
Ishikawa <danay.bizhan@gmail.com>
2022-11-08 20:09:03 +0600

    proverka svyazi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Negri pidorasi");
+        System.out.println("Hello world! Negri pidorasi(net)");
     }
 }
